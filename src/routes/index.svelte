c89959a3f20e7e5ed15883e16d330711ec058246
Philip Holden <philip.holden@woodmac.com>
2020-01-19 17:38:31 +0000

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,77 @@
+<style>
+  h1,
+  figure,
+  p {
+    text-align: center;
+    margin: 0 auto;
+  }
+
+  h1 {
+    font-size: 2.8em;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
+
+  figure {
+    margin: 0 0 1em 0;
+  }
+
+  img {
+    width: 100%;
+    max-width: 400px;
+    margin: 0 0 1em 0;
+  }
+
+  p {
+    margin: 1em auto;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
+</style>
+
+<svelte:head>
+  <title>omysoul.io</title>
+</svelte:head>
+
+<h1>omysoul</h1>
+
+<p>
+  Thanks for visiting. We are trying to gather a large number of people to
+  worship in public spaces around the world. We are looking for the first
+  pioneers willing to step out. If you think this might be you please sign up
+  for the newsletter.
+</p>
+
+<p>
+  If you live in Edinburgh UK please put that in the location box and I will let
+  you know about the first open air events. We will try to connect clusters of
+  people in other areas.
+</p>
+
+<iframe
+  width="540"
+  height="960"
+  src="https://8958425c.sibforms.com/serve/MUIEAMKAHraziho8bSNnrHK8FdhYSq0XMeXtpLQ0s8MSSKU8U0Y4KkOFXU40q14cmzFumK23xvL2yRm47L8yObKoJEeGy-9dOLmXLbkCXmDukgIPnSBsqirj-RP7_1USwpBJhAARVK7-kawixBNK6cTur2Zmbn1gF_xUzkVa8iFuHmMKjSvRLIdrcFwD5fgRYcrHrYdVR-affTFz"
+  frameborder="0"
+  scrolling="auto"
+  allowfullscreen
+  style="display: block;margin-left: auto;margin-right: auto;max-width: 100%;" />
+
+<p>
+  Once I have a guide for you to set up your free song server I will announce it
+  in the newsletter. For the time being if you have comments or questions post
+  them in the comments on the YouTube video.
+</p>
+
+<iframe
+  style="margin: 1em auto; display: block"
+  width="560"
+  height="315"
+  src="https://www.youtube.com/embed/ty8BoC2Tly8"
+  frameborder="0"
+  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+  allowfullscreen />
